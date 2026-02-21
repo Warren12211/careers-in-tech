efb524e49c7ffb3ed7ce28ca875af38058d10f03
Warren Fowler <warrenf0122@gmail.com>
2026-02-21 17:58:24 -0500

    organized code and started stuff for adding more content to website

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// var currentCard = ;
+
+
+// const card1 = document.getElementById("card1");
+// const card2 = document.getElementById("card2");
+// const card3 = document.getElementById("card3");
+// const card4 = document.getElementById("card4");
+// const card5 = document.getElementById("card5");
+// const card6 = document.getElementById("card6");
+
+// var card1Pos = (currentCard + 1) % 6
+// var card2Pos = (currentCard + 2) % 6
+// var card3Pos = (currentCard + 3) % 6
+// var card4Pos = (currentCard + 4) % 6
+// var card5Pos = (currentCard + 5) % 6
+// var card6Pos = (currentCard + 6) % 6
+// var card1Pos = (currentCard + 1) % 6
+
+
+
+
+
+
+
+
+// card1.style.order = card1Pos
+// card2.style.order = card2Pos
+// card3.style.order = card3Pos
+// card4.style.order = card4Pos
+// card5.style.order = card5Pos
+// card6.style.order = card6Pos
+
+
+
+
+
